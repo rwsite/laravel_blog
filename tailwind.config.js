33bcd12d89034d9f2e839e48bb84f31dd63b2b94
Aleksei Tikhomirov <alex@rwsite.ru>
2024-02-17 00:54:24 +0300

    template files updated

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,7 @@
 import defaultTheme from 'tailwindcss/defaultTheme';
 import forms from '@tailwindcss/forms';
+import typography from '@tailwindcss/typography';
+
 
 /** @type {import('tailwindcss').Config} */
 export default {
@@ -11,12 +13,52 @@ export default {
     ],
 
     theme: {
+        screens: {
+            'xs': '375px',
+            'sm': '540px',
+            'md': '720px',
+            'lg': '960px',
+            'xl': '1140px',
+            '2xl': '1550px',
+        },
+        container: {
+            center: true,
+            padding: '20px',
+        },
+        fontFamily: {
+            'sans': ['Gilroy', 'sans-serif'],
+        },
+        fontSize: {
+            'xxs': ['14px', '1.6em'],
+            'xs': ['16px', '1.6em'],
+            'sm': ['18px', '1.6em'],
+            'md': ['20px', '1.45em'],
+            'lg': ['26px', '1.3em'],
+            'xl': ['36px', '1.3em'],
+            '2xl': ['64px', '1.1em'],
+            '3xl': ['96px', '1.1em'],
+        },
         extend: {
+            colors: {
+                white: "#FFF",
+                purple: "#7843E9",
+                pink: "#EC4176",
+                dark: "#222",
+                gray: "#454545",
+                darkblue: "#1E1F43",
+                body: '#BDBECA',
+                card: '#323359',
+            },
             fontFamily: {
                 sans: ['Figtree', ...defaultTheme.fontFamily.sans],
             },
         },
     },
 
-    plugins: [forms],
+    plugins: [
+        forms,
+        typography,
+        require('@tailwindcss/aspect-ratio'),
+        require('@tailwindcss/line-clamp'),
+    ],
 };
